9f44ec811ab6bda19f35f45a2422d2faf9beac3e
Velizar <31567122+VelizarBG@users.noreply.github.com>
2023-12-12 00:07:06 +0200

    Fix incorrect history order
    - Also ditch nested File constructors for Path.resolve()

--- src/main/java/velizarbg/clientfunctions/ClientFunctionsClient.java
+++ src/main/java/velizarbg/clientfunctions/ClientFunctionsClient.java
@@ -13,6 +13,8 @@ import velizarbg.clientfunctions.gui.ClientFunctionScreen;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.Comparator;
+import java.util.List;
 
 public class ClientFunctionsClient implements ClientModInitializer {
 	private final KeyBinding openGui = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.clientfunctions.gui", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_B, "key.categories.clientfunctions"));
@@ -31,9 +33,11 @@ public class ClientFunctionsClient implements ClientModInitializer {
 			if (Constants.CONFIG.historyMode != ModConfig.HistoryMode.RESTART)
 				return;
 
-			File dir = new File(new File(client.runDirectory, "config"), "clientfunctions");
+			File dir = client.runDirectory.toPath().resolve("config/clientfunctions").toFile();
 			if ((dir.exists() && dir.isDirectory()) || dir.mkdirs()) {
-				for(File functionFile : FileUtils.listFiles(dir, new String[]{"mcfunction"}, false)) {
+				List<File> files = (List<File>) FileUtils.listFiles(dir, new String[]{"mcfunction"}, false);
+				files.sort(Comparator.comparing(file -> file.getName().length())); // listFiles() only sorts them alphabetically, so we tweak it a bit (refer to https://stackoverflow.com/a/50637385)
+				for(File functionFile : files) {
 					try {
 						ClientFunctionScreen.textBoxHistory.add(FileUtils.readFileToString(functionFile, Charset.defaultCharset()));
 					} catch (IOException ioException) {
@@ -48,7 +52,7 @@ public class ClientFunctionsClient implements ClientModInitializer {
 			if (Constants.CONFIG.historyMode != ModConfig.HistoryMode.RESTART)
 				return;
 
-			File dir = new File(new File(client.runDirectory, "config"), "clientfunctions");
+			File dir = client.runDirectory.toPath().resolve("config/clientfunctions").toFile();
 			if ((dir.exists() && dir.isDirectory()) || dir.mkdirs()) {
 				try {
 					FileUtils.cleanDirectory(dir);
